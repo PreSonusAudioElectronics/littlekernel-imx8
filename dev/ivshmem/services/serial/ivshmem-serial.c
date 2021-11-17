71a635e0faf75dec0fe07646851e2d4a800939ae
danderson-presonus <danderson@presonus.com>
2021-11-16 17:05:17 -0800

    putchar through circular buffer working

--- dev/ivshmem/services/serial/ivshmem-serial.c
+++ dev/ivshmem/services/serial/ivshmem-serial.c
@@ -5,20 +5,53 @@
 #include "ivshmem-serial.h"
 #include <debug.h>
 #include <assert.h>
-// #include <lib/klog.h>
 #include <stdio.h>
+#include <lib/cbuf.h>
+#include <lk/init.h>
+#include <lib/io.h>
+#include <string.h>
+#include <kernel/mutex.h>
+#include <lib/console.h>
+#include <string.h>
+
+struct ivshm_serial {
+    mutex_t tx_lock;
+    cbuf_t tx_cbuf;
+    thread_t *thread;
+    bool running;
+    struct ivshm_endpoint *ep;
+};
+
+#ifndef IVSHM_SERIAL_BUFFER_SIZE
+#define IVSHM_SERIAL_BUFFER_SIZE (16 * 1024)
+#endif
+#ifndef IVSHM_LOGGER_BUF_SIZE
+#define IVSHM_LOGGER_BUF_SIZE 4096
+#endif
 
 static struct ivshm_endpoint *ep_serial;
+static char ivshm_serial_buf[IVSHM_SERIAL_BUFFER_SIZE];
+
+#define IVSHM_SERIAL_TX_BUF_SIZE (64)
+static char txbuf[IVSHM_SERIAL_TX_BUF_SIZE];
+
+static struct ivshm_serial _ivshm_serial;
+static struct ivshm_serial *_con = &_ivshm_serial;
 
 #define MIN(x, y) ((x < y) ? x : y)
 
-#define IVSHM_SERIAL_ECHO_BUF_SIZE (512)
-
-#include <lib/console.h>
-#include <string.h>
 static void ivshm_start_serial(struct ivshm_endpoint *);
 static void ivshm_stop_serial(struct ivshm_endpoint *);
 
+static inline void write_chars(struct ivshm_endpoint *ep, char *buf, unsigned len)
+{
+    ASSERT( len <= IVSHM_SERIAL_TX_BUF_SIZE);
+    struct ivshm_ep_buf ep_buf;
+    memcpy(txbuf, buf, len);
+    ivshm_ep_buf_init(&ep_buf);
+    ivshm_ep_buf_add(&ep_buf, txbuf, len);
+    ivshm_endpoint_write(ep, &ep_buf);
+}
 
 static ssize_t ivshm_serial_consume(struct ivshm_endpoint *ep, struct ivshm_pkt *pkt)
 {
@@ -29,13 +62,10 @@ static ssize_t ivshm_serial_consume(struct ivshm_endpoint *ep, struct ivshm_pkt 
           len, strnlen(payload, len), payload);
 
     // echo back
-    struct ivshm_ep_buf ep_buf;
-    static char buf[IVSHM_SERIAL_ECHO_BUF_SIZE];
-    memcpy(buf, payload, len);
-    ivshm_ep_buf_init(&ep_buf);
-    ivshm_ep_buf_add(&ep_buf, buf, len);
-    ivshm_endpoint_write(ep, &ep_buf);
-
+    for(unsigned i=0; i<len; ++i)
+    {
+        ivshm_putchar(payload[i]);
+    }
     return 0;
 }
 
@@ -63,32 +93,41 @@ void ivshm_exit_serial(struct ivshm_info *info)
     ivshm_endpoint_destroy(ep_serial);
 }
 
-#include <lk/init.h>
-#include <lib/cbuf.h>
-#include <lib/io.h>
-#include <string.h>
+inline int ivshm_putchar(char c)
+{
+    // If buffer is empty, send directly
+    size_t used = cbuf_space_used(&_con->tx_cbuf);
+    if( 0 == used )
+    {
+        write_chars(ep_serial, &c, 1);
+        return 1;
+    }
 
-struct ivshm_serial {
-    print_callback_t print_cb;
-    spin_lock_t tx_lock;
-    cbuf_t tx_buf;
-    thread_t *thread;
-    bool running;
-    struct ivshm_endpoint *ep;
-};
+    // Otherwise, if buffer is full return ERR and drop char
+    size_t avail = cbuf_space_avail(&_con->tx_cbuf);
+    if( 0 == avail )
+    {
+        return -1;
+    }
 
-#ifndef IVSHM_SERIAL_BUFFER_SIZE
-#define IVSHM_SERIAL_BUFFER_SIZE (16 * 1024)
-#endif
-#ifndef IVSHM_LOGGER_BUF_SIZE
-#define IVSHM_LOGGER_BUF_SIZE 4096
-#endif
+    // Otherwise, put char onto buff
+    return cbuf_write_char(&_con->tx_cbuf, c, false);
+}
 
+inline int ivshm_getchar_noblock(char *out)
+{
+    return 0;
+}
 
-static char ivshm_serial_buf[IVSHM_SERIAL_BUFFER_SIZE];
+void ivshm_acquire_lock(void)
+{
+    mutex_acquire(&_con->tx_lock);
+}
 
-static struct ivshm_serial _ivshm_serial;
-static struct ivshm_serial *_con = &_ivshm_serial;
+void ivshm_release_lock(void)
+{
+    mutex_release(&_con->tx_lock);
+}
 
 static int ivshm_serial_thread(void *arg)
 {
@@ -99,24 +138,23 @@ static int ivshm_serial_thread(void *arg)
     size_t payload_sz;
 
     while (con->running) 
-    {
+    {      
         thread_yield();
-        // ivshm_ep_buf_init(&ep_buf);
-        // event_wait(&con->tx_buf.event);
-        // payload_sz = cbuf_peek(&con->tx_buf, regions);
-        // DEBUG_ASSERT(payload_sz > 0);
-        // ivshm_ep_buf_add(&ep_buf, regions[0].iov_base, regions[0].iov_len);
-        // if (regions[1].iov_len)
-        //     ivshm_ep_buf_add(&ep_buf, regions[1].iov_base, regions[1].iov_len);
+        ivshm_ep_buf_init(&ep_buf);
+        event_wait(&con->tx_cbuf.event);
+        payload_sz = cbuf_peek(&con->tx_cbuf, regions);
+        DEBUG_ASSERT(payload_sz > 0);
+        ivshm_ep_buf_add(&ep_buf, regions[0].iov_base, regions[0].iov_len);
+        if (regions[1].iov_len)
+            ivshm_ep_buf_add(&ep_buf, regions[1].iov_base, regions[1].iov_len);
 
-        // ivshm_endpoint_write(con->ep, &ep_buf);
+        ivshm_endpoint_write(con->ep, &ep_buf);
 
-        // cbuf_read(&con->tx_buf, NULL, payload_sz, false);
+        cbuf_read(&con->tx_cbuf, NULL, payload_sz, false);
     }
 
     return 0;
 }
-
 
 static void ivshm_start_serial(struct ivshm_endpoint *ep)
 {
@@ -147,24 +185,13 @@ static void ivshm_stop_serial(struct ivshm_endpoint *ep)
     thread_join(con->thread, &retcode, 1000);
 }
 
-static void ivshm_serial_print(print_callback_t *cb, const char *str, size_t len)
-{
-    struct ivshm_serial *con = cb->context;
-    cbuf_write(&con->tx_buf, str, len, false);
-}
-
 static void ivshm_hook_serial_init(unsigned level)
 {
     memset(_con, 0x0, sizeof(*_con));
 
-    cbuf_initialize_etc(&_con->tx_buf, IVSHM_SERIAL_BUFFER_SIZE, &ivshm_serial_buf);
+    cbuf_initialize_etc(&_con->tx_cbuf, IVSHM_SERIAL_BUFFER_SIZE, &ivshm_serial_buf);
 
-    spin_lock_init(&_con->tx_lock);
-
-    _con->print_cb.print = ivshm_serial_print;
-    _con->print_cb.context = _con;
-
-    register_print_callback(&_con->print_cb);
+    mutex_init(&_con->tx_lock);
 }
 
 LK_INIT_HOOK(ivshm_serial, ivshm_hook_serial_init, LK_INIT_LEVEL_PLATFORM_EARLY - 1);
