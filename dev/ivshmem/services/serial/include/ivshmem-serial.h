ad7027d3bc6cf1d67f5e0d3d5c06c388308919b0
danderson-presonus <danderson@presonus.com>
2021-11-17 23:08:27 -0800

    checkpoint: instantiated driver via device tree (not quite fully working)

--- dev/ivshmem/services/serial/include/ivshmem-serial.h
+++ dev/ivshmem/services/serial/include/ivshmem-serial.h
@@ -8,6 +8,19 @@ struct ivshm_info;
 int ivshm_init_serial(struct ivshm_info *);
 void ivshm_exit_serial(struct ivshm_info *);
 
+/*!
+ * \brief Put one char into an ivshmem serial stream
+ * 
+ * \param id Endpoint id of the ivshm serial path
+ * \param c The char to sent
+ * \return int The actual number of bytes sent
+ */
+int ivshm_putchar(unsigned id, char c);
+
+int ivshm_getchar_noblock(char *out);
+void ivshm_acquire_lock(void);
+void ivshm_release_lock(void);
+
 #endif //__IVSHMEM_SERIAL_H
 
 
--- dev/ivshmem/services/serial/ivshmem-serial.c
+++ dev/ivshmem/services/serial/ivshmem-serial.c
@@ -9,48 +9,51 @@
 #include <lib/cbuf.h>
 #include <lk/init.h>
 #include <lib/io.h>
+#include <lib/appargs.h>
 #include <string.h>
 #include <kernel/mutex.h>
 #include <lib/console.h>
-#include <string.h>
+#include <dev/driver.h>
+#include <dev/ivshm.h>
+#include <err.h>
 
-struct ivshm_serial {
+#include <string.h>
+#include <stdlib.h>
+
+struct ivshm_serial_service {
+    struct list_node node;
+    unsigned id;
+    struct ivshm_endpoint *ept;
     mutex_t tx_lock;
     cbuf_t tx_cbuf;
     thread_t *thread;
     bool running;
-    struct ivshm_endpoint *ep;
+    char *tx_buf;
 };
 
-#ifndef IVSHM_SERIAL_BUFFER_SIZE
-#define IVSHM_SERIAL_BUFFER_SIZE (16 * 1024)
-#endif
-#ifndef IVSHM_LOGGER_BUF_SIZE
-#define IVSHM_LOGGER_BUF_SIZE 4096
-#endif
-
-static struct ivshm_endpoint *ep_serial;
-static char ivshm_serial_buf[IVSHM_SERIAL_BUFFER_SIZE];
+#define IVSHM_MAX_CBUF_SIZE (1024 * 64)
 
 #define IVSHM_SERIAL_TX_BUF_SIZE (64)
-static char txbuf[IVSHM_SERIAL_TX_BUF_SIZE];
 
-static struct ivshm_serial _ivshm_serial;
-static struct ivshm_serial *_con = &_ivshm_serial;
+static struct list_node serial_service_list =
+        LIST_INITIAL_VALUE(serial_service_list);
 
-#define MIN(x, y) ((x < y) ? x : y)
+/*!
+ * \brief and event for signalling when the ivshmem is ready to run
+ */
+static event_t ivshm_ready_evt = EVENT_INITIAL_VALUE(ivshm_ready_evt, 0, 0);
+
 
 static void ivshm_start_serial(struct ivshm_endpoint *);
-static void ivshm_stop_serial(struct ivshm_endpoint *);
 
-static inline void write_chars(struct ivshm_endpoint *ep, char *buf, unsigned len)
+static inline void write_chars(struct ivshm_serial_service *service, char *buf, unsigned len)
 {
     ASSERT( len <= IVSHM_SERIAL_TX_BUF_SIZE);
     struct ivshm_ep_buf ep_buf;
-    memcpy(txbuf, buf, len);
+    memcpy(service->tx_buf, buf, len);
     ivshm_ep_buf_init(&ep_buf);
-    ivshm_ep_buf_add(&ep_buf, txbuf, len);
-    ivshm_endpoint_write(ep, &ep_buf);
+    ivshm_ep_buf_add(&ep_buf, service->tx_buf, len);
+    ivshm_endpoint_write(service->ept, &ep_buf);
 }
 
 static ssize_t ivshm_serial_consume(struct ivshm_endpoint *ep, struct ivshm_pkt *pkt)
@@ -61,57 +64,108 @@ static ssize_t ivshm_serial_consume(struct ivshm_endpoint *ep, struct ivshm_pkt 
     printf("payload : %lu bytes strlen %lu, content: %s",
           len, strnlen(payload, len), payload);
 
-    // echo back
     for(unsigned i=0; i<len; ++i)
     {
-        ivshm_putchar(payload[i]);
+        ivshm_putchar(ep->id, payload[i]);
     }
     return 0;
 }
 
 int ivshm_init_serial(struct ivshm_info *info)
 {
-    ep_serial = ivshm_endpoint_create(
-                 "ivshm_serial",
-                 IVSHM_EP_ID_SERIAL,
-                 ivshm_serial_consume,
-                 info,
-                 8 * 1024,
-                 0
-             );
+    event_signal(&ivshm_ready_evt, false);
 
-    DEBUG_ASSERT(NULL != ep_serial );
+    // ep_serial = ivshm_endpoint_create(
+    //              "ivshm_serial",
+    //              IVSHM_EP_ID_SERIAL,
+    //              ivshm_serial_consume,
+    //              info,
+    //              8 * 1024,
+    //              0
+    //          );
 
-    ivshm_start_serial(ep_serial);
+    // DEBUG_ASSERT(NULL != ep_serial );
+
+    // ivshm_start_serial(ep_serial);
 
     return 0;
 }
 
 void ivshm_exit_serial(struct ivshm_info *info)
 {
-    ivshm_stop_serial(ep_serial);
-    ivshm_endpoint_destroy(ep_serial);
+    // ivshm_stop_serial(ep_serial);
+    // ivshm_endpoint_destroy(ep_serial);
+    struct ivshm_serial_service *service, *next;
+    int ret = 0;
+
+    list_for_every_entry_safe(&serial_service_list, service, next,
+        struct ivshm_serial_service, node) {
+            service->running = false;
+            thread_join(service->thread, &ret, 5000);
+            // mutex_destroy(&service->tx_lock);
+            free(service->tx_cbuf.buf);
+            ivshm_endpoint_destroy(service->ept);
+            list_delete(&service->node);
+            free(service);
+        }
 }
 
-inline int ivshm_putchar(char c)
+static inline struct ivshm_serial_service
+                        *_ivshm_serial_get_service(unsigned id)
 {
+    struct ivshm_serial_service *service;
+
+    list_for_every_entry(&serial_service_list, service,
+                         struct ivshm_serial_service, node) 
+    {
+        if (service->id == id)
+            return service;
+    }
+
+    printlk(LK_ERR, "%s:%d: Could not find serial-%d endpoint\n",
+            __PRETTY_FUNCTION__, __LINE__, id);
+    return NULL;
+}
+
+int ivshm_putchar(unsigned id, char c)
+{
+    struct ivshm_serial_service *service;
+    int ret = 0;
+
+    service = _ivshm_serial_get_service(id);
+    if(!service) {
+        return ERR_NOT_FOUND;
+    }
+
+    // get the tx_cbuf for this id
+    cbuf_t *cbuf = &service->tx_cbuf;
+
+    // get the lock
+    mutex_acquire(&service->tx_lock);
+
     // If buffer is empty, send directly
-    size_t used = cbuf_space_used(&_con->tx_cbuf);
+    size_t used = cbuf_space_used(cbuf);
     if( 0 == used )
     {
-        write_chars(ep_serial, &c, 1);
-        return 1;
+        write_chars(service, &c, 1);
+        ret = 1;
+        goto unlock;
     }
 
     // Otherwise, if buffer is full return ERR and drop char
-    size_t avail = cbuf_space_avail(&_con->tx_cbuf);
+    size_t avail = cbuf_space_avail(cbuf);
     if( 0 == avail )
     {
-        return -1;
+        ret = -1;
+        goto unlock;
     }
 
     // Otherwise, put char onto buff
-    return cbuf_write_char(&_con->tx_cbuf, c, false);
+    ret = cbuf_write_char(cbuf, c, false);
+
+unlock:
+    mutex_release(&service->tx_lock);
+    return ret;
 }
 
 inline int ivshm_getchar_noblock(char *out)
@@ -119,38 +173,28 @@ inline int ivshm_getchar_noblock(char *out)
     return 0;
 }
 
-void ivshm_acquire_lock(void)
-{
-    mutex_acquire(&_con->tx_lock);
-}
-
-void ivshm_release_lock(void)
-{
-    mutex_release(&_con->tx_lock);
-}
-
-static int ivshm_serial_thread(void *arg)
+static int ivshm_serial_tx_thread(void *arg)
 {
 
-    struct ivshm_serial *con = arg;
+    struct ivshm_serial_service *service = arg;
     struct ivshm_ep_buf ep_buf;
     iovec_t regions[2];
     size_t payload_sz;
 
-    while (con->running) 
+    while (service->running) 
     {      
-        thread_yield();
         ivshm_ep_buf_init(&ep_buf);
-        event_wait(&con->tx_cbuf.event);
-        payload_sz = cbuf_peek(&con->tx_cbuf, regions);
+        event_wait(&service->tx_cbuf.event);
+        payload_sz = cbuf_peek(&service->tx_cbuf, regions);
         DEBUG_ASSERT(payload_sz > 0);
         ivshm_ep_buf_add(&ep_buf, regions[0].iov_base, regions[0].iov_len);
         if (regions[1].iov_len)
             ivshm_ep_buf_add(&ep_buf, regions[1].iov_base, regions[1].iov_len);
 
-        ivshm_endpoint_write(con->ep, &ep_buf);
+        ivshm_endpoint_write(service->ept, &ep_buf);
 
-        cbuf_read(&con->tx_cbuf, NULL, payload_sz, false);
+        cbuf_read(&service->tx_cbuf, NULL, payload_sz, false);
+        thread_yield();
     }
 
     return 0;
@@ -158,42 +202,154 @@ static int ivshm_serial_thread(void *arg)
 
 static void ivshm_start_serial(struct ivshm_endpoint *ep)
 {
+    
 
-    struct ivshm_serial *con = _con;
+    // struct ivshm_serial *con = _con;
 
-    con->thread = thread_create(
-                    "ivshm_serial_thread",
-                    ivshm_serial_thread,
-                    (void *) con,
-                    LOW_PRIORITY - 1,
-                    IVSHM_LOGGER_BUF_SIZE +  4096
-                 );
+    // con->thread = thread_create(
+    //                 "ivshm_serial_thread",
+    //                 ivshm_serial_thread,
+    //                 (void *) con,
+    //                 LOW_PRIORITY - 1,
+    //                 IVSHM_LOGGER_BUF_SIZE +  4096
+    //              );
 
-    con->running = true;
-    con->ep = ep;
-    smp_wmb();
-    thread_resume(con->thread);
+    // con->running = true;
+    // con->ep = ep;
+    // smp_wmb();
+    // thread_resume(con->thread);
 }
 
-static void ivshm_stop_serial(struct ivshm_endpoint *ep)
-{
-    struct ivshm_serial *con = _con;
-    int retcode;
+// static void ivshm_stop_serial(struct ivshm_endpoint *ep)
+// {
+//     struct ivshm_serial *con = _con;
+//     int retcode;
 
-    con->running = false;
-    smp_wmb();
-    thread_join(con->thread, &retcode, 1000);
-}
+//     con->running = false;
+//     smp_wmb();
+//     thread_join(con->thread, &retcode, 1000);
+// }
 
 static void ivshm_hook_serial_init(unsigned level)
 {
-    memset(_con, 0x0, sizeof(*_con));
+    // memset(_con, 0x0, sizeof(*_con));
 
-    cbuf_initialize_etc(&_con->tx_cbuf, IVSHM_SERIAL_BUFFER_SIZE, &ivshm_serial_buf);
+    // cbuf_initialize_etc(&_con->tx_cbuf, IVSHM_SERIAL_BUFFER_SIZE, &ivshm_serial_buf);
 
-    mutex_init(&_con->tx_lock);
+    // mutex_init(&_con->tx_lock);
 }
 
-LK_INIT_HOOK(ivshm_serial, ivshm_hook_serial_init, LK_INIT_LEVEL_PLATFORM_EARLY - 1);
+static inline void print_err_property(char *property_name)
+{
+    printlk(LK_ERR, "%s:%d: Endpoint %s property cannot be read, aborting!\n",
+        __PRETTY_FUNCTION__, __LINE__, property_name);
+}
+
+static status_t ivshm_serial_dev_init(struct device *dev)
+{
+    int ret = NO_ERROR;
+    char *property_name = NULL;
+    char * cbuf_mem = NULL;
+    char name[IVSHM_EP_MAX_NAME];
+    uint32_t id, ep_size;
+    struct ivshm_serial_service *service = NULL;
+
+    struct ivshm_dev_data *ivshm_dev = ivshm_get_device(0);
+    if(!ivshm_dev) {
+        return ERR_NOT_READY;
+    }
+
+    struct ivshm_info *info = ivshm_dev->handler_arg;
+    if (!info) {
+        return ERR_NOT_READY;
+    }
+
+    property_name = (char*)"id";
+    ret = of_device_get_int32(dev, property_name, &id);
+    if(ret) {
+        print_err_property(property_name);
+        return ERR_NOT_FOUND;
+    }
+
+    property_name = (char*)"size";
+    ret = of_device_get_int32(dev, property_name, &ep_size);
+    if(ret) {
+        print_err_property(property_name);
+        return ERR_NOT_FOUND;
+    }
+
+    DEBUG_ASSERT(ep_size < IVSHM_MAX_CBUF_SIZE);
+
+    service = malloc(sizeof(struct ivshm_serial_service));
+    if(!service) {
+        ret = ERR_NO_MEMORY;
+        goto cleanup;
+    }
+    
+    memset(service, 0, sizeof(struct ivshm_serial_service));
+
+    service->id = id;
+    snprintf(name, IVSHM_EP_MAX_NAME, "serial-%u", service->id);
+    service->ept = ivshm_endpoint_create(
+            name,
+            service->id,
+            ivshm_serial_consume,
+            info,
+            ep_size,
+            0 // fixme
+        );
+
+    // Setup tx thread to transmit queued buffers
+    char const * prefix = "ivshm-serial";
+    char name_buf[32];
+    size_t len = strnlen(prefix, 32);
+    memcpy(name_buf, prefix, len);
+    snprintf(name_buf + len, 32-len, "%u", id);
+    service->thread = thread_create(
+        name_buf,
+        ivshm_serial_tx_thread,
+        (void*)service,
+        IVSHM_EP_GET_PRIO(service->ept->id),
+        DEFAULT_STACK_SIZE );
+
+    // initialize tx circ buffer
+    cbuf_mem = malloc(ep_size);
+    if(!cbuf_mem) {
+        ret = ERR_NO_MEMORY;
+        goto cleanup;
+    }
+    cbuf_initialize_etc(&service->tx_cbuf, ep_size, cbuf_mem);
+
+    // initialize tx buffer
+    service->tx_buf = malloc(IVSHM_SERIAL_TX_BUF_SIZE);
+    if(!service->tx_buf) {
+        ret = ERR_NO_MEMORY;
+        goto cleanup;
+    }
+
+    service->running = true;
+    thread_resume(service->thread);
+    list_add_tail(&serial_service_list, &service->node);
+
+    return ret;
+
+cleanup:
+    if(service->tx_buf) { free(service->tx_buf); }
+    if(service->ept) { ivshm_endpoint_destroy(service->ept); }
+    if(service->thread) {
+        thread_join(service->thread, NULL, 5000);
+        free(service->thread);
+    }
+    if(service) { free(service); }
+
+    return ret;
+}
+
+static struct driver_ops the_ops = {
+    .init = ivshm_serial_dev_init
+};
+
+// LK_INIT_HOOK(ivshm_serial, ivshm_hook_serial_init, LK_INIT_LEVEL_PLATFORM_EARLY - 1);
+DRIVER_EXPORT_WITH_LVL(ivshm_serial, &the_ops, DRIVER_INIT_CORE);
 
 
