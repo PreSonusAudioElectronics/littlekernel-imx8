fc5ba55aa14a95ff86c0cf647e38283dc5c6f5d1
Bryan Audiffred <audiffred@mac.com>
2021-11-19 13:13:49 -0500

    switch lk build to submodule approach

--- app/external/rules.mk
+++ app/external/rules.mk
@@ -8,11 +8,11 @@ MODULE_SRCS += \
 
 # Guide to external libraries
 EXTRA_LINK_LIBS += app pep
-GLOBAL_LDFLAGS += -L../../ \
-                  -L../../lib/pep \
+GLOBAL_LDFLAGS += -L../build/ \
+                  -L../build/lib/pep \
 
 # Set dependencies on the library outputs
-EXTRA_LINKER_DEPS += ../../libapp.a \
-                     ../../lib/libpep.a \
+EXTRA_LINKER_DEPS += ../build/libapp.a \
+                     ../build/lib/pep/libpep.a \
 
 include make/module.mk
