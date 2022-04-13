00a7f71cbe2659cff2fc701965ad50bbe71232ce
danderson-presonus <danderson@presonus.com>
2022-04-13 09:30:21 -0700

    adjust jstart.sh and jkill.sh to work correctly with virt eth adapter

--- scripts/jkill.sh
+++ scripts/jkill.sh
@@ -3,15 +3,6 @@
 echo 'Shutdown virtual ethernet adapter'
 rmmod virtual_eth_adapter
 
-echo 'Shutdown rpmsg_ivshmem adapter'
-rmmod rpmsg_ivshmem_adapter
-
-echo 'Shutdown rpmsg_console'
-rmmod rpmsg_console
-
-echo 'Shutdown ivshmem'
-rmmod ivshmem
-
 echo 'Shutdown lk cell'
 jailhouse cell shutdown lk
 
